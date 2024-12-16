7d8e14ba4669344c6be4a9f32f9d848a234c0630
anthony-kidd <akidd19@ivytech.edu>
2024-12-16 16:36:44 -0500

    added hello-world.py

---
+++ Day 1/hello-world.py
@@ -0,0 +1,8 @@
+"""
+Day 1 of 100 Days of Code
+Name: hello-world.py
+Author: Anthony Kidd
+Purpose: This program prints "Hello World!" 
+12-16-2024 - File created & base program completed.
+"""
+print("Hello World!")
