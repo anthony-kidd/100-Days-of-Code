9acff8753adcc89cc340f2b86fd3157d66af0784
anthony-kidd <akidd19@ivytech.edu>
2024-12-16 16:47:42 -0500

    Added input-from-console.py to Day 1

---
+++ Day 1/input-from-console.py
@@ -0,0 +1,9 @@
+"""
+Day 1 of 100 Days of Code
+Name: input-from-console.py
+Author: Anthony Kidd
+Purpose: This program gets user input from the console and prints a simple hello message
+12-16-2024 - File created & base program completed.
+"""
+name = input("What is your name? ")
+print("Hello, " + name + ". It is a pleasure to meet you!")
